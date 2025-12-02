f446fcea053d08ac7387be601488118509ad26b3
Komo <komoyon@gmail.com>
2025-12-02 10:47:11 +0900

    commit 반영, 추가 수정

--- src/magic.c
+++ src/magic.c
@@ -335,8 +335,9 @@ void spell_sunburst(byte level, struct char_data *ch,
 		dam >>= 1;
 	if (number(1, 15) == 1)
 		spell_blindness(level, ch, victim, 0);
+	/* commented out, 251129
 	sprintf(buf, "DEBUG: sunburst: %d\n", dam);
-	log(buf);
+	log(buf); */
 
 	damage(ch, victim, dam, SPELL_SUNBURST);
 }
