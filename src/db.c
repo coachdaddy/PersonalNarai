71cfd137683a7e7faa12386fdc30e63a0e4ba84e
Komo <komoyon@gmail.com>
2025-12-04 18:15:57 +0900

    setup_dir 잘못 수정되어 다시 수정

--- src/db.c
+++ src/db.c
@@ -661,54 +661,44 @@ void reload_world_file(FILE *fl, int zone_rnum)
 /* with CodeRabbit, 251204 */
 void setup_dir(FILE *fl, int room, int dir)
 {
-	int tmp;
-	char err_buf[256];
+    int tmp;
+    char err_buf[256];
 
-	CREATE(world[room].dir_option[dir], struct room_direction_data, 1);
+    CREATE(world[room].dir_option[dir], struct room_direction_data, 1);
 
-	world[room].dir_option[dir]->general_description = fread_string(fl);
-	if (!world[room].dir_option[dir]->general_description) {
-        snprintf(err_buf, sizeof(err_buf), "SYSERR: NULL description in setup_dir (Room #%d, Dir %d)", world[room].number, dir);
-        log(err_buf);
-        exit(1);
-    }
+    world[room].dir_option[dir]->general_description = fread_string(fl);
+    world[room].dir_option[dir]->keyword = fread_string(fl);
 
-	world[room].dir_option[dir]->keyword = fread_string(fl);
-	if (!world[room].dir_option[dir]->keyword) {
-        snprintf(err_buf, sizeof(err_buf), "SYSERR: NULL keyword in setup_dir (Room #%d, Dir %d)", world[room].number, dir);
-        log(err_buf);
-        exit(1);
-    }
-
-	if (fscanf(fl, " %d ", &tmp) != 1) {
+    if (fscanf(fl, " %d ", &tmp) != 1) {
         snprintf(err_buf, sizeof(err_buf), "SYSERR: Format error in setup_dir flags (Room #%d)", world[room].number);
         log(err_buf);
         exit(1);
     }
-	if (tmp == 1)
-		world[room].dir_option[dir]->exit_info = EX_ISDOOR;
-	else if (tmp == 2)
-		world[room].dir_option[dir]->exit_info = EX_ISDOOR | EX_PICKPROOF;
-	else if (tmp == 3)
-		world[room].dir_option[dir]->exit_info = EX_ISDOOR | EX_NOPHASE;
-	else if (tmp == 4)
-		world[room].dir_option[dir]->exit_info = EX_ISDOOR | EX_PICKPROOF | EX_NOPHASE;
-	else
-		world[room].dir_option[dir]->exit_info = 0;
 
-	if (fscanf(fl, " %d ", &tmp) != 1) {
+    if (tmp == 1)
+        world[room].dir_option[dir]->exit_info = EX_ISDOOR;
+    else if (tmp == 2)
+        world[room].dir_option[dir]->exit_info = EX_ISDOOR | EX_PICKPROOF;
+    else if (tmp == 3)
+        world[room].dir_option[dir]->exit_info = EX_ISDOOR | EX_NOPHASE;
+    else if (tmp == 4)
+        world[room].dir_option[dir]->exit_info = EX_ISDOOR | EX_PICKPROOF | EX_NOPHASE;
+    else
+        world[room].dir_option[dir]->exit_info = 0;
+
+    if (fscanf(fl, " %d ", &tmp) != 1) {
         snprintf(err_buf, sizeof(err_buf), "SYSERR: Format error in setup_dir key (Room #%d)", world[room].number);
         log(err_buf);
         exit(1);
     }
-	world[room].dir_option[dir]->key = tmp;
+    world[room].dir_option[dir]->key = tmp;
 
-	if (fscanf(fl, " %d ", &tmp) != 1) {
+    if (fscanf(fl, " %d ", &tmp) != 1) {
         snprintf(err_buf, sizeof(err_buf), "SYSERR: Format error in setup_dir to_room (Room #%d)", world[room].number);
         log(err_buf);
         exit(1);
     }
-	world[room].dir_option[dir]->to_room = tmp;
+    world[room].dir_option[dir]->to_room = tmp;
 }
 
 void renum_world(void)
