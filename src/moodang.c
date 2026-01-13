def3f34425dcde2cc573de6fa641f0741bf86a38
coachdaddy <sisyphos@gmail.com>
2026-01-13 08:39:47 -0800

    great potion

--- src/moodang.c
+++ src/moodang.c
@@ -21,6 +21,7 @@ struct char_data *jangsambong;
 
 #define TAICHI_STEP		(jangsambong->quest.solved)
 
+#define GREAT_POTION 18001
 #define TAICHI_ORB 18002
 #define YANG_ORB 2839
 #define YANG_SEED 17060
@@ -44,6 +45,31 @@ int jang_sambong_func(struct char_data *ch, int cmd, char *arg)
 	}
 
  	return 0;
+}
+
+int great_potion(struct char_data *ch, int cmd, char *arg)
+{
+	char buf[100];
+	struct obj_data *obj;
+
+	if (cmd != 12)
+		return FALSE;
+
+	/* eat */
+	one_argument(arg, buf);
+	if (!(obj = get_obj_in_list_vis(ch, buf, ch->carrying)))
+		return FALSE;
+
+	if (obj_index[obj->item_number].virtual != GREAT_POTION)
+		return FALSE;
+
+	send_to_char("갑자기 새로운 힘이 느껴집니다.\n\r", ch);
+
+	// GET_HIT(ch) = GET_MAX_HIT(ch) * 1.8 ;
+
+	extract_obj(obj);
+
+	return TRUE;
 }
 
 int taichi_orb(struct char_data *ch, int cmd, char *arg)
